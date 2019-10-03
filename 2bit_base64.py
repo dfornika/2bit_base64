71b17e4ecf0cafbab2744863affef3e1065ccc4d
Dan Fornika <dfornika@gmail.com>
2019-10-03 16:52:49 -0700

    Abandon twobitreader library

--- 2bit_base64.py
+++ 2bit_base64.py
@@ -3,18 +3,32 @@
 import argparse
 import os
 import sys
-import twobitreader
 
 from pprint import pprint
 
+
+def char_to_bits(char):
+    char_to_bits_dict = {
+        'T': 0b00,
+        'C': 0b01,
+        'A': 0b10,
+        'G': 0b11,
+    }
+    try:
+        output = char_to_bits_dict[char]
+    except KeyError as e:
+        output = None
+    return output
+
 def main(args):
-    twobitfile = twobitreader.TwoBitFile(args.twobit)
-    for twobitseq in twobitfile:
-        print(twobitfile[twobitseq])
+    seq = 'ACGATGCAT'
+    
+    output = list(map(char_to_bits, list(seq)))
+    pprint(output)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("twobit", help="2bit file")
+    parser.add_argument("fasta", help="fasta file")
     args = parser.parse_args()
     main(args)
